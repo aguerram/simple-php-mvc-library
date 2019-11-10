654a597f13a06057fcce44d9c5f1a1e1364d0f88
Mostafa <agurram2013@gmail.com>
2019-11-10 20:40:09 +0100

    assets

--- config/functions.php
+++ config/functions.php
@@ -5,3 +5,8 @@
         global $env;
         return $env->get("APP_URL").$path;
     }
+    function assets($path)
+    {
+        global $env;
+        return $env->get("APP_URL").$path;
+    }
--- views/login.php
+++ views/login.php
@@ -7,6 +7,7 @@
 
 <head>
   <title>Login Page</title>
+  <link rel="stylesheet" href="<?= assets('/assets/css/login.css') ?>" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
@@ -32,12 +33,6 @@
                 <span class="input-group-text"><i class="fas fa-key"></i></span>
               </div>
               <input min="8" type="password" name="password" class="form-control input_pass" value="" placeholder="Password">
-            </div>
-            <div class="form-group">
-              <div class="custom-control custom-checkbox">
-                <input name="remember_me" type="checkbox" class="custom-control-input" id="customControlInline">
-                <label class="custom-control-label" for="customControlInline">Remember me</label>
-              </div>
             </div>
             <div class="d-flex justify-content-center mt-3 login_container">
               <button type="submit" name="button" class="btn login_btn">Login</button>
